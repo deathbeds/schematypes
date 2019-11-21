374add45672a3992a7c71cca129cc9fd04a617fb
Tony Fast <tony.fast@gmail.com>
2019-11-21 16:27:17 -0500

    Update lists.py

--- schematypes/lists.py
+++ schematypes/lists.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-import importnb
-
-with importnb.Notebook():
-    try:
-        from .schemas import List, Dict
-        from .strings import Uri, String
-    except:
-        from schemas import List, Dict
-        from strings import Uri, String
-
-
-# In[2]:
-
+from .schemas import List, Dict
+from .strings import Uri, String
 
 class ListofList(List, additionalProperties=List.__schema__):
     ...
@@ -24,9 +10,6 @@ class ListofList(List, additionalProperties=List.__schema__):
 
 class ListofDict(List, additionalProperties=Dict.__schema__):
     ...
-
-
-# In[3]:
 
 
 class ListofUri(List, items=Uri.__schema__, minItems=1):
@@ -56,43 +39,3 @@ def aiorun(object):
             return aiorun(object)
         else:
             raise e
-
-
-# In[5]:
-
-
-class Test(__import__("unittest").TestCase):
-    def test_instances(x):
-        ...
-
-
-# In[6]:
-
-
-def load_tests(loader, tests, ignore):
-    tests.addTests(
-        doctest.DocTestSuite(
-            importlib.import_module(__name__), optionflags=doctest.ELLIPSIS
-        )
-    )
-    return tests
-
-
-if __name__ == "__main__":
-    import unittest, pytest, jsonschema, importlib, doctest
-
-    unittest.main(argv=" ", exit=False, verbosity=1)
-
-
-# In[11]:
-
-
-if __name__ == "__main__":
-    get_ipython().system("jupyter nbconvert --to script lists.ipynb")
-    get_ipython().system("black lists.py")
-    get_ipython().system("pyreverse lists -osvg -plists")
-    display(__import__("IPython").display.SVG("classes_lists.svg"))
-    get_ipython().system("rm classes_lists.svg lists.py")
-
-
-# In[ ]:
