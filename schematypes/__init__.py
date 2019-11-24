e23142aa0e30686ae958b6532e0e4bdc00aadba3
Tony Fast <tony.fast@gmail.com>
2019-11-24 11:22:54 -0500

    Update __init__.py

--- schematypes/__init__.py
+++ schematypes/__init__.py
@@ -1,4 +1,7 @@
 __all__ = ("discover",)
 with __import__("importnb").Notebook():
+    from .dicts import *
+    from .lists import *
+    from .objects import *
+    from .strings import *
     from .schemas import *
-    from . import dicts, lists, objects, strings
