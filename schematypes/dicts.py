b16e480db8f3315b39bbd9f381ef5f2e2cba6560
Tony Fast <tony.fast@gmail.com>
2019-11-24 11:09:29 -0500

    Update dicts.py

--- schematypes/dicts.py
+++ schematypes/dicts.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 
 from .schemas import Dict, List
-import requests
-
 
 class DictofDict(Dict, additionalProperties=Dict.__schema__): ...
 
@@ -14,7 +12,7 @@ class Test(__import__("unittest").TestCase):
 # class NbFormat(Dict, **__import__('nbformat').validator._get_schema_json(nbformat.v4)): 
 #     __context__ = 'https://raw.githubusercontent.com/jupyter/nbformat/master/nbformat/v4/nbformat.v4.schema.json#/'
 
-class JsonPatch(List, **requests.get("http://json.schemastore.org/json-patch").json()):
-    def __call__(self, object): return Type.discover(__import__('jsonpatch').apply_patch(object, self))
-class TableSchema(Dict, **requests.get("https://frictionlessdata.io/schemas/table-schema.json").json()): ...
-class GeoJson(Dict, **requests.get("http://json.schemastore.org/geojson").json()): ...
+# class JsonPatch(List, **requests.get("http://json.schemastore.org/json-patch").json()):
+#     def __call__(self, object): return Type.discover(__import__('jsonpatch').apply_patch(object, self))
+# class TableSchema(Dict, **requests.get("https://frictionlessdata.io/schemas/table-schema.json").json()): ...
+# class GeoJson(Dict, **requests.get("http://json.schemastore.org/geojson").json()): ...
